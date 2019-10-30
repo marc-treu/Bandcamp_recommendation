e8a9e5d5c4858a3fb1f1903c4151457349a61478
marc-treu <mr.treu@gmail.com>
2019-10-30 18:35:09 +0100

    Add more attribute for describe a fan, add a repr function

--- BandCampAPI/Fan.py
+++ BandCampAPI/Fan.py
@@ -2,10 +2,18 @@
 
 class Fan:
 
-    def __init__(self, id, name, adr):
+    def __init__(self, id, name, adr, location, bio, fav_genre, length_collection, length_wishlist):
         self.id = id
         self.name = name
         self.adr = adr
+        self.location = location
+        self.bio = bio
+        self.fav_genre = fav_genre
+        self.length_collection = length_collection
+        self.length_wishlist = length_wishlist
 
     def get_collection(self):
         pass
+
+    def __repr__(self):
+        return str(self.__dict__)
