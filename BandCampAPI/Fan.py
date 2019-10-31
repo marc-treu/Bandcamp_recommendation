c9855c88ac493f8cc78d16cc0cce4036943f6a3d
marc-treu <mr.treu@gmail.com>
2019-10-31 17:59:41 +0100

    add getters

--- BandCampAPI/Fan.py
+++ BandCampAPI/Fan.py
@@ -2,10 +2,10 @@
 
 class Fan:
 
-    def __init__(self, id, name, adr, location, bio, fav_genre, length_collection, length_wishlist):
+    def __init__(self, id, name, url, location, bio, fav_genre, length_collection, length_wishlist):
         self.id = id
         self.name = name
-        self.adr = adr
+        self.url = url
         self.location = location
         self.bio = bio
         self.fav_genre = fav_genre
@@ -14,6 +14,18 @@ class Fan:
 
     def get_collection(self):
         pass
+
+    def get_id(self):
+        return self.id
+
+    def get_url(self):
+        return self.url
+
+    def get_length_collection(self):
+        return self.length_collection
+
+    def get_length_wishlist(self):
+        return self.length_wishlist
 
     def __repr__(self):
         return str(self.__dict__)
