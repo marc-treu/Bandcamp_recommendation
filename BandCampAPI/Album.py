d2f0972fbec7c1093c86779eb4fe40793e1f971e
marc-treu <mr.treu@gmail.com>
2019-10-31 18:00:52 +0100

    add class for albums

---
+++ BandCampAPI/Album.py
@@ -0,0 +1,24 @@
+
+
+class Album:
+
+    def __init__(self, band_id, band_name, album_id, album_name, url, genre_id, collected_count):
+        self.band_id = band_id
+        self.band_name = band_name
+        self.album_id = album_id
+        self.album_name = album_name
+        self.url = url
+        self.genre_id = genre_id
+        self.collected_count = collected_count
+
+    def get_collection(self):
+        pass
+
+    def get_band_id(self):
+        return self.band_id
+
+    def get_url(self):
+        return self.url
+
+    def __repr__(self):
+        return str(self.__dict__)
