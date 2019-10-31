b0c14c667ed6761ef33303b2341bdba4b99ac396
marc-treu <mr.treu@gmail.com>
2019-10-31 18:39:49 +0100

    add a function that scrape the fan discography

--- BandCampAPI/FanScroller.py
+++ BandCampAPI/FanScroller.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 from BandCampAPI.Fan import Fan
+from BandCampAPI.Album import Album
 
 
 class FanScroller:
@@ -21,3 +22,34 @@ class FanScroller:
                   data['collection_data']['item_count'], data['wishlist_data']['item_count'])
 
         return fan
+
+    @staticmethod
+    def get_discography(fan):
+
+        s = requests.session()
+
+        header = {'Host': 'bandcamp.com', 'Referer': fan.get_url()}
+
+        payload_col = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
+                       "count": fan.get_length_collection()}
+
+        collection_items = s.post('https://bandcamp.com/api/fancollection/1/collection_items',
+                                  data=json.dumps(payload_col), headers=header).json()
+
+        result = []
+
+        for item in collection_items['items']:
+            result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
+                                item['item_url'], item['genre_id'], item['also_collected_count']))
+
+        payload_wish = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
+                        "count": fan.get_length_wishlist()}
+
+        wishlist_items = s.post('https://bandcamp.com/api/fancollection/1/wishlist_items',
+                                data=json.dumps(payload_wish), headers=header).json()
+
+        for item in wishlist_items['items']:
+            result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
+                                item['item_url'], item['genre_id'], item['also_collected_count']))
+
+        return result
