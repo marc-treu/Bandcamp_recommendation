8df4dd0e1279c1d11050a825a75250464db351b5
marc-treu <mr.treu@gmail.com>
2019-10-31 19:14:01 +0100

    Large modification, we suppress the object for only method, all methodes were static

--- BandCampAPI/FanScroller.py
+++ BandCampAPI/FanScroller.py
@@ -6,50 +6,47 @@ from BandCampAPI.Fan import Fan
 from BandCampAPI.Album import Album
 
 
-class FanScroller:
+def get_data_initialise(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return False
+    soup = BeautifulSoup(r.content, features="html.parser")
 
-    @staticmethod
-    def get_data_initialise(url):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return False
-        soup = BeautifulSoup(r.content, features="html.parser")
+    data = json.loads(soup.find('div', {'id': 'pagedata'})['data-blob'])
 
-        data = json.loads(soup.find('div', {'id': 'pagedata'})['data-blob'])
+    fan = Fan(data['fan_data']['fan_id'], data['fan_data']['username'], data['fan_data']['trackpipe_url'],
+              data['fan_data']['location'], data['fan_data']['bio'], data['fan_data']['fav_genre'],
+              data['collection_data']['item_count'], data['wishlist_data']['item_count'])
 
-        fan = Fan(data['fan_data']['fan_id'], data['fan_data']['username'], data['fan_data']['trackpipe_url'],
-                  data['fan_data']['location'], data['fan_data']['bio'], data['fan_data']['fav_genre'],
-                  data['collection_data']['item_count'], data['wishlist_data']['item_count'])
+    return fan
 
-        return fan
 
-    @staticmethod
-    def get_discography(fan):
+def get_discography(fan):
 
-        s = requests.session()
+    s = requests.session()
 
-        header = {'Host': 'bandcamp.com', 'Referer': fan.get_url()}
+    header = {'Host': 'bandcamp.com', 'Referer': fan.get_url()}
 
-        payload_col = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
-                       "count": fan.get_length_collection()}
+    payload_col = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
+                   "count": fan.get_length_collection()}
 
-        collection_items = s.post('https://bandcamp.com/api/fancollection/1/collection_items',
-                                  data=json.dumps(payload_col), headers=header).json()
+    collection_items = s.post('https://bandcamp.com/api/fancollection/1/collection_items',
+                              data=json.dumps(payload_col), headers=header).json()
 
-        result = []
+    result = []
 
-        for item in collection_items['items']:
-            result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
-                                item['item_url'], item['genre_id'], item['also_collected_count']))
+    for item in collection_items['items']:
+        result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
+                            item['item_url'], item['genre_id'], item['also_collected_count']))
 
-        payload_wish = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
-                        "count": fan.get_length_wishlist()}
+    payload_wish = {"fan_id": fan.get_id(), "older_than_token": "9999999999::a::",
+                    "count": fan.get_length_wishlist()}
 
-        wishlist_items = s.post('https://bandcamp.com/api/fancollection/1/wishlist_items',
-                                data=json.dumps(payload_wish), headers=header).json()
+    wishlist_items = s.post('https://bandcamp.com/api/fancollection/1/wishlist_items',
+                            data=json.dumps(payload_wish), headers=header).json()
 
-        for item in wishlist_items['items']:
-            result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
-                                item['item_url'], item['genre_id'], item['also_collected_count']))
+    for item in wishlist_items['items']:
+        result.append(Album(item['band_id'], item['band_name'], item['album_id'], item['album_title'],
+                            item['item_url'], item['genre_id'], item['also_collected_count']))
 
-        return result
+    return result
