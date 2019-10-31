d653faca33e619ed8a7aaddfb3753756932c2897
marc-treu <mr.treu@gmail.com>
2019-10-31 19:21:41 +0100

    refactoriasation of the name FanScroller for Scrapper

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
-from BandCampAPI import Fan, FanScroller
+from BandCampAPI import Fan, Scrapper
 from Utils.utils import *
 from Interface_terminal.interface import *
 
 
 def create_fan():
 
-    fan = FanScroller.get_data_initialise(formate_url(welcome()))
-    res = FanScroller.get_discography(fan)
+    fan = Scrapper.get_data_initialise(formate_url(welcome()))
+    res = Scrapper.get_discography(fan)
     fan.fill(res)
     create_data_base()
     save_data(fan, fan.get_name())
