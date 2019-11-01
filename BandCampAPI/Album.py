c0e69133a1f7803e68508ebe9c95d5df11f3525a
marc-treu <mr.treu@gmail.com>
2019-11-01 12:33:52 +0100

    add a url for the band

--- BandCampAPI/Album.py
+++ BandCampAPI/Album.py
@@ -2,12 +2,13 @@
 
 class Album:
 
-    def __init__(self, band_id, band_name, album_id, album_name, url, genre_id, collected_count):
+    def __init__(self, band_id, band_name, band_url, album_id, album_name, album_url, genre_id, collected_count):
         self.band_id = band_id
         self.band_name = band_name
+        self.band_url = band_url
         self.album_id = album_id
         self.album_name = album_name
-        self.url = url
+        self.album_url = album_url
         self.genre_id = genre_id
         self.collected_count = collected_count
         self.fans = False
@@ -18,11 +19,14 @@ class Album:
     def get_band_id(self):
         return self.band_id
 
+    def get_band_url(self):
+        return self.band_url
+
     def get_album_id(self):
         return self.album_id
 
-    def get_url(self):
-        return self.url
+    def get_album_url(self):
+        return self.album_url
 
     def __repr__(self):
         return str(self.__dict__)
